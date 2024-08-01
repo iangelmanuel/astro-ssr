013d162b85d22545c2d4bb8ea80ed6f51940170a
iangelmanuel <iangelmanueldm@gmail.com>
2024-08-01 14:50:09 -0500

    chore: Update tsconfig.json, package.json, and astro.config.mjs
    This commit updates the tsconfig.json file to include additional compiler options for strictNullChecks, baseUrl, paths, and jsx. It also updates the package.json file to add new dependencies for @astrojs/vue, canvas-confetti, lodash.debounce, and @types/canvas-confetti, @types/lodash.debounce. Additionally, the astro.config.mjs file is modified to import the vue integration from @astrojs/vue. These changes enhance the configuration and functionality of the project.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@ import { defineConfig } from "astro/config"
 import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
 import cloudflare from "@astrojs/cloudflare"
-
 import db from "@astrojs/db"
+
+import vue from "@astrojs/vue"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), db()],
+  integrations: [mdx(), sitemap(), db(), vue()],
   output: "hybrid",
   adapter: cloudflare()
 })
