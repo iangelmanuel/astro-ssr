fdbbe2845ca3e31adbe8cf46d26879a85b5e48b1
iangelmanuel <iangelmanueldm@gmail.com>
2024-08-01 09:38:11 -0500

    chore: Update npm dependencies and Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
 import cloudflare from "@astrojs/cloudflare"
 
+import db from "@astrojs/db"
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), db()],
   output: "hybrid",
   adapter: cloudflare()
 })
